e3d1a12905466a746bf04fdf766d819f862ec880
damianTrybuszek <damian.trybuszek@gmail.com>
2021-11-16 00:15:11 +0100

    Comments and offers classes, repos, controllers and servicess started to created. Still working on them.

--- server/src/main/java/com/example/zadyszke/comment/Comment.java
+++ server/src/main/java/com/example/zadyszke/comment/Comment.java
@@ -1,4 +1,44 @@
 package com.example.zadyszke.comment;
 
+
+import com.example.zadyszke.offer.Offer;
+import lombok.*;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy=IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Offer offer;
+
+    private String content;
+    private String author;
+    private LocalDateTime creationDateTime;
+    private LocalDateTime modifyDateTime;
+
+    public void modify(Comment newData) {
+        if (StringUtils.isNotBlank(newData.getContent())) {
+            this.setContent(newData.getContent());
+            this.setModifyDateTime(LocalDateTime.now());
+        }
+
+        if(StringUtils.isNotBlank(newData.getAuthor())) {
+            this.setAuthor(newData.getAuthor());
+            this.setModifyDateTime(LocalDateTime.now());
+        }
+    }
+
 }
--- server/src/main/java/com/example/zadyszke/comment/CommentRepository.java
+++ server/src/main/java/com/example/zadyszke/comment/CommentRepository.java
@@ -1,4 +1,6 @@
 package com.example.zadyszke.comment;
 
-public interface CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
