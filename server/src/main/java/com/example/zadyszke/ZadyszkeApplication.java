ec34bdb6e564930057d857be7c50a2ca87c98f99
Loilei <marcelina.gasiorek@gmail.com>
2021-11-21 00:07:54 +0100

    Password hashing added (Spring Security)

--- server/src/main/java/com/example/zadyszke/ZadyszkeApplication.java
+++ server/src/main/java/com/example/zadyszke/ZadyszkeApplication.java
@@ -2,8 +2,10 @@ package com.example.zadyszke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ZadyszkeApplication {
 
 	public static void main(String[] args) {
