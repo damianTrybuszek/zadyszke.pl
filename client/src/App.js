c80cb5d264daa7c1c067c6277eaf118164f40622
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-12-01 13:43:49 +0100

    Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Component } from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+        <div className="App">
+          <Navbar/>
+        </div>
+    )
+  }
+
 }
 
 export default App;
