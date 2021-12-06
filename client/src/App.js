b7bac2700eb3e0ce8d2ae7bca84ee30821f0d248
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-12-07 00:50:48 +0100

    Footer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { Component } from "react";
 import { Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import MainNotLoggedInPage from "./pages/MainNotLoggedIn";
 import MainLoggedInPage from "./pages/MainLoggedIn";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ED8627",
+      white: "#FFFFFF"
+    },
+    secondary: {
+      main: "#2F303A",
+      white: "#FFFFFF"
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#000000"
+    }
+  },
+  typography: {
+    fontFamily: "Poppins"
+  }
+});
 
 class App extends Component {
   constructor(props) {
@@ -16,12 +37,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Routes>
-          <Route exact path="/" element={<MainNotLoggedInPage />} />
-          <Route path="/logged-in" element={<MainLoggedInPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route exact path="/" element={<MainNotLoggedInPage />} />
+            <Route path="/logged-in" element={<MainLoggedInPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </ThemeProvider>
       </div>
     );
   }
---
+++ client/src/components/FooterCategories.js
@@ -0,0 +1,94 @@
+import { Typography } from "@mui/material";
+import { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+class FooterCategories extends Component {
+  constructor(props) {
+    super(props);
+    this.copyright = <div></div>;
+  }
+
+  render(props) {
+    return (
+      <Container>
+        <Row >
+          <Col item xs={2}>
+            <Row>
+              <Typography variant="h5">Kategorie</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Kategoria 1</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Kategoria 2</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Kategoria 3</Typography>
+            </Row>
+          </Col>
+          <Col item xs={2}>
+            <Row>
+              <Typography variant="h5">O nas</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">O nas 1</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">O nas 2</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">O nas 3</Typography>
+            </Row>
+          </Col>
+          <Col item xs={2}>
+            <Row>
+              <Typography variant="h5">Wsparcie</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Wsparcie 1</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Wsparcie 2</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Wsparcie 3</Typography>
+            </Row>
+          </Col>
+          <Col item xs={3}>
+            <Row>
+              <Typography variant="h5">Warunki Użytkowania</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Warunki Użytkowania 1</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Warunki Użytkowania 2</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Warunki Użytkowania 3</Typography>
+            </Row>
+          </Col>
+          <Col item xs={3}>
+            <Row>
+              <Typography variant="h5">Polityka Prywatności</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Polityka Prywatności 1</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Polityka Prywatności 2</Typography>
+            </Row>
+            <Row>
+              <Typography variant="subtitle1">Polityka Prywatności 3</Typography>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col>{this.props.copyright}</Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default FooterCategories;
