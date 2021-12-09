844b077ec32264f5320ea1c05040b22421acaa88
damianTrybuszek <damian.trybuszek@gmail.com>
2021-12-09 01:01:19 +0100

    login form in progress

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import NavbarTop from "../components/NavbarTop";
+import LoginForm from "../components/LoginForm";
 
 class LoginPage extends Component {
   render() {
     return (
       <div>
         <NavbarTop />
-        <h1>Login page</h1>
+        <LoginForm />
       </div>
     );
   }
