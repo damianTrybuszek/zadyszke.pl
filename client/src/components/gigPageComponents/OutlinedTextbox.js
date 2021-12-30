f6402ad5b2065bdcb69fceadfe2dfe134c6a0218
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-12-30 01:38:12 +0100

    Create gig page 3 elements added

---
+++ client/src/components/gigPageComponents/OutlinedTextbox.js
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+class OutlinedText extends Component {
+  constructor(props) {
+    super(props);
+    this.title = "";
+  }
+  render() {
+    return (
+      <Box sx={{ border: 1, borderRadius: 3, padding: 1 }}>
+        <TextField
+          variant="outlined"
+          size="small"
+          sx={{ label: { color: "white" }, width: 250 }}
+          name="title"
+          required
+          id="title"
+          label={this.props.title}
+          autoFocus
+        />
+      </Box>
+    );
+  }
+}
+
+export default OutlinedText;
